cdc9122951ec4766e1ab869ea6c383da2e7ff5b4
chaymamighri <chaymamighri942@gmail.com>
2025-10-22 22:51:37 +0200

    Refactor user model and add login logic with JWT authentication and password encryption

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 
 const app = express();
