6a698a9e250ce48cb87a38691314b5cb8032eadc
rayen <challoufrayen09@gmail.com>
2025-11-05 17:08:01 +0100

    added crud to normal users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,13 +4,13 @@ import bcrypt from "bcryptjs";
 
 export const createUser = async (req, res) => {
   try {
-    const { name, lastname,cin ,email, password } = req.body;
+    const { name, lastname, cin, email, password, role, banque, rib } = req.body;
 
     if (!name || !lastname || !cin || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    const newUser = new User({ name, lastname, cin ,email, password });
+    const newUser = new User({ name, lastname, cin, email, password, role, banque, rib });
     await newUser.save();
 
     res.status(201).json({ message: " User created", user: newUser });
@@ -32,6 +32,10 @@ export const loginUser = async (req, res) => {
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(400).json({ message: "Email or password invalid" });
+    }
+
+    if ((user.role === "formateur" || user.role === "coordinateur") && user.status !== "approuvé") {
+      return res.status(403).json({ message: "Compte en attente de validation", status: user.status });
     }
 
     // Generate a JWT token
@@ -58,3 +62,78 @@ export const loginUser = async (req, res) => {
 
   }
 };
+//delete user
+export const DeleteUser = async (req, res) => {
+  try {
+    const { id } = req.params; 
+    const result = await User.deleteOne({ _id: id });
+
+    if (result.deletedCount > 0) {
+      return res.status(200).json({
+        message: "User deleted successfully",
+      });
+    } else {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur", error: error.message  });
+
+}};
+//Update User
+export const UpdateUser=async(req,res)=>{
+  try {
+    const {id}=req.params;
+    const UpdatedData=req.body;
+
+    const existingUser = await User.findById(id);
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Prevent update if status is "en-attente"
+    if (existingUser.status === "en-attente") {
+      return res.status(400).json({
+        message: "Can't update pending users",
+      });
+    }
+
+    const user=await User.findByIdAndUpdate(id,UpdatedData, {
+      new: true, 
+      runValidators: true, 
+    })
+
+    res.status(200).json(
+      {
+        message:"User Updated successfully",
+        user,
+      }
+    )
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur", error: error.message  });
+
+    
+  }
+
+}
+//get user by id
+export const getUser=async(req,res)=>{
+  try {
+    const {id}=req.params;
+    const user=await User.findById(id);
+    if(!user){
+      res.status(404).json({
+        message:"User Not Found"
+      })
+    }
+    res.status(200).json({
+      user,
+    })
+
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur", error: error.message  });
+
+    
+  }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, DeleteUser, getUser, loginUser, UpdateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.put("/updateUser/:id",UpdateUser);
+router.delete("/deleteUser/:id",DeleteUser);
+router.get("/getUserById/:id",getUser);
 
 export default router;
