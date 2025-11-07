66088f6856204e567f899674ac020b93c5beb4a0
rayen <challoufrayen09@gmail.com>
2025-11-07 07:02:25 +0100

    added reset password comptable

--- backend/routes/ResetPasswordRoutes.js
+++ backend/routes/ResetPasswordRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import {SendForgotPasswordLink,VerifyResetPasswordToken,ResetPassword} from "../controllers/RestPasswordController.js"
+import {SendForgotPasswordLink,ResetPassword} from "../controllers/RestPasswordController.js"
 
 const ResetPasswordRoutes = express.Router();
 ResetPasswordRoutes.post("/getForgotPasswordLink",SendForgotPasswordLink);
-ResetPasswordRoutes.get("/verifyToken/:id/:token",VerifyResetPasswordToken);
-ResetPasswordRoutes.post("/resetPassword/:id/:token",ResetPassword);
+ResetPasswordRoutes.post("/resetPassword/:type/:id/:token",ResetPassword);
 
 
 export default ResetPasswordRoutes;
